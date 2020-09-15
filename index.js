dea9daacddb1f6e7a5c9bdf79152a0fcc3f08132
joan <sjlouji10@gmail.com>
2020-09-15 11:47:56 +0530

    Admin Bro added

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ var morgan = require('morgan')
 const mongoose = require('mongoose');
 const Emp = require('./Models/emp')
 const bodyParser  = require('body-parser');
-
+const AdminBro = require('admin-bro')
+const AdminBroMongoose = require('@admin-bro/mongoose')
+const AdminBroExpress = require('@admin-bro/express')
 //Routes
 app.use(morgan('combined'))
 app.use('/',dashborad)
 app.use('/blogs',blogs)
 app.use('/settings',settings)
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/add', function(req,res) {
   const emp = new Emp({
     empName: req.body.empName,
@@ -26,7 +26,15 @@ app.post('/add', function(req,res) {
   })
 })
 
+AdminBro.registerAdapter(AdminBroMongoose)
+const User = mongoose.model('User', { name: String, email: String, surname: String })
+const AdminBroOptions = {
+  resources: [User, Emp],
+}
+const adminBro = new AdminBro(AdminBroOptions)
 
+const router = AdminBroExpress.buildRouter(adminBro)
+app.use(adminBro.options.rootPath, router)
 
 //Database
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
